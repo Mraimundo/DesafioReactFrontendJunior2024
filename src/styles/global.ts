24ab9619f6cea112883c0731ecd3cf2e8a9dc9a7
Mraimundo <mdumundo18@gmail.com>
2024-06-19 23:04:03 -0300

    config: config routes and global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,15 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
+
   body {
     background: ${(props) => props.theme["bg"]};
     -webkit-font-smoothing: antialiased;
@@ -19,10 +28,23 @@ export const GlobalStyle = createGlobalStyle`
   button {
     border: 0;
     background: transparent;
+    cursor: pointer;
   }
 
-  body, input, textarea, button {
-    font-family: "Inter",  sans-serif;
+  body,
+  input,
+  textarea,
+  button {
+    font: 16px "Inter", sans-serif;
   }
 
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 700;
+  }
 `;
