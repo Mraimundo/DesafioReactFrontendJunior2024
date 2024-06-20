46f261b54d12fd2325afde13802eb41f54e0ae6c
Mraimundo <mdumundo18@gmail.com>
2024-06-20 18:45:14 -0300

    feat: integrate context to app

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,19 @@
-import React from "react";
 import { ThemeProvider } from "styled-components";
+import { Toaster } from "sonner";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { AppProvider } from "./contexts";
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-
-      <section>
-        <h1>Todos</h1>
-      </section>
+      <AppProvider>
+        <Home />
+        <GlobalStyle />
+        <Toaster richColors />
+      </AppProvider>
     </ThemeProvider>
   );
 }
